fa1225afb5c296752ca3a130cf85ebc907eb8a31
Earthchild2 <nthabiseng.fakude2@gmail.com>
2024-07-08 11:05:22 +0200

    added function to change background colour

---
+++ script.js
@@ -0,0 +1,58 @@
+
+    let elements = document.getElementById('blue');
+    elements.addEventListener("click",bluefn)
+
+function bluefn() {
+ let backc = document.getElementById('blue')
+ document.body.style.background = "#2d89cf"  
+}
+
+let elementg = document.getElementById('green');
+    elementg.addEventListener("click",greenfn)
+
+function greenfn() {
+        let backc = document.getElementById('green')
+        document.body.style.background = "#32a838"  
+}
+
+let elemento= document.getElementById('orange');
+    elemento.addEventListener("click",orangefn)
+
+function orangefn() {
+        let backc = document.getElementById('orange')
+        document.body.style.background = "#ebae34"
+        
+    } 
+    let elementp= document.getElementById('pink');
+    elementp.addEventListener("click",pinkfn)
+
+function pinkfn() {
+        let backc = document.getElementById('pink')
+        document.body.style.background = "#eb34d3"
+        
+    } 
+
+    let element = document.getElementById('purple');
+    element.addEventListener("click",purplefn)
+
+function purplefn() {
+        let backc = document.getElementById('purple')
+        document.body.style.background = "#a834eb"
+        
+    } 
+
+
+
+
+
+
+
+
+ 
+  
+
+
+
+
+
+
